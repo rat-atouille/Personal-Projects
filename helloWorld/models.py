b60d314f245975d8ebe9ca6ba5f76eec75aae3f4
rat-atouille <heenap0113@gmail.com>
2024-06-12 14:26:05 -0400

    changes

--- helloWorld/models.py
+++ helloWorld/models.py
@@ -1,11 +1,9 @@
 from django.db import models
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-  username = models.OneToOneField(User, on_delete=models.CASCADE)
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
   name = models.CharField(max_length=20)  
   password = models.CharField(max_length=20)
   email = models.EmailField(max_length=50)
--- helloWorld/views.py
+++ helloWorld/views.py
@@ -1,21 +1,45 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from helloWorld.models import Profile
 
 # Create your views here.
 
 # open main page
 def index(request):
- #if "tasks" not in request.session:
-  # request.session["tasks"]=[]
- return render(request, "web/main.html")
+    return render(request, "web/main.html")
 
 # open shelf page
 def shelf(request):
-  return render(request, "web/shelf.html")
+    return render(request, "web/shelf.html")
 
-# open login 
+# open login page
 def login(request):
-  return render(request, "web/login.html")
+    return render(request, "web/login.html")
 
 def signup(request):
-  return render(request, "web/signup.html")
+    if request.method == 'POST':
+        name = request.POST['uname']
+        username = request.POST['uid']
+        email = request.POST['uemail']
+        password1 = request.POST['psw1']
+        password2 = request.POST['psw2']
+
+        if password1 == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, "Email already exists")
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already exists')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password1)
+                user.first_name = name
+                user.save()
+                new_profile = Profile.objects.create(user=user)
+                new_profile.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Passwords do not match')
+            return redirect('signup')
+    return render(request, "web/signup.html")
